c8c68c39ff453e7c04006c3b53934a21a4557072
KDanila <concista@gmail.com>
2020-06-17 09:57:27 +0300

    Add KdTree realisation.

--- Part_5/src/main/java/ru/kdv/kd_trees/KdTree.java
+++ Part_5/src/main/java/ru/kdv/kd_trees/KdTree.java
@@ -1,4 +1,62 @@
 package ru.kdv.kd_trees;
 
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+
 public class KdTree {
+    private int size;
+    private Node root;
+
+    private class Node {
+        private Point2D point;
+        private RectHV rectangle;
+        private Node left;
+        private Node right;
+    }
+
+    // construct an empty set of points
+    public KdTree() {
+        size = 0;
+        root = null;
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // number of points in the set
+    public int size() {
+        return size;
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+
+    }
 }
